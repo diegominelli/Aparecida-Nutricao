40922b39f3eddb967ae6dc7291f133cd63513444
Diego Minelli <diego.minelli@hotmail.com>
2021-10-07 11:32:38 -0300

    Calculando o imc

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var titulo = document.querySelector('.titulo');
+titulo.textContent = 'Aparecida Nutricionista';
+
+var paciente = document.querySelector('#primeiro-paciente');
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+var resultado = document.querySelector('.info-imc');
+resultado.textContent = imc;
+
+console.log(imc);
