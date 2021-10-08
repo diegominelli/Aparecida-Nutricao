bd75d9d544742ac142b90f6334da81af0e00797b
Diego Minelli <diego.minelli@hotmail.com>
2021-10-08 17:49:18 -0300

    Validando os campos do formulario

--- js/form.js
+++ js/form.js
@@ -10,17 +10,28 @@ botaoAdicionar.addEventListener('click', function (event) {
   var erros = validaPaciente(paciente);
 
   if (erros.length > 0) {
-    var mensagemErro = document.querySelector('#mensagem-erro');
-    mensagemErro.textContent = erros;
+    exibeMensagensDeErro(erros);
     return;
   }
 
   var tabela = document.querySelector('#tabela-pacientes');
-
+  console.log(erros);
   tabela.appendChild(pacienteTr);
 
   form.reset();
+  var mensagensErro = document.querySelector('#mensagens-erro');
+  mensagensErro.innerHTML = '';
 });
+
+function exibeMensagensDeErro(erros) {
+  var ul = document.querySelector('#mensagens-erro');
+  ul.innerHTML = '';
+  erros.forEach(function (erro) {
+    var li = document.createElement('li');
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
+}
 
 function obtemPacienteDoFormulario(form) {
   var paciente = {
@@ -58,12 +69,28 @@ function montaTd(dado, classe) {
 function validaPaciente(paciente) {
   var erros = [];
 
+  if (paciente.nome.length == 0) {
+    erros.push('O nome não pode ser em branco');
+  }
+
   if (!validaPeso(paciente.peso)) {
     erros.push('Peso é inválido');
   }
 
   if (!validaAltura(paciente.altura)) {
     erros.push('Altura é inválida');
+  }
+
+  if (paciente.gordura.length == 0) {
+    erros.push('A gordura não pode ser em branco');
+  }
+
+  if (paciente.peso.length == 0) {
+    erros.push('O peso não pode ser em branco');
+  }
+
+  if (paciente.altura.length == 0) {
+    erros.push('A altura não pode ser em branco');
   }
 
   return erros;
