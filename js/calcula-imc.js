7cc4df446648506ca328f653352823753bd58f66
Diego Minelli <diego.minelli@hotmail.com>
2021-10-08 16:16:15 -0300

    Desenvolvendo as funcoes para validacao nos campos do formulario

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -14,17 +14,17 @@ for (var i = 0; i < pacientes.length; i++) {
 
   var resultado = paciente.querySelector('.info-imc');
 
-  var pesoValido = true;
-  var alturaValida = true;
+  var pesoValido = validaPeso(peso);
+  var alturaValida = validaAltura(altura);
 
-  if (peso <= 0 || peso >= 1000) {
+  if (!pesoValido) {
     console.log('Peso inválido!');
     pesoValido = false;
     resultado.textContent = 'Peso inválido!';
     paciente.classList.add('paciente-invalido');
   }
 
-  if (altura <= 0 || altura >= 3.0) {
+  if (!alturaValida) {
     console.log('Altura inválida!');
     alturaValida = false;
     resultado.textContent = 'Altura inválida!';
@@ -34,6 +34,22 @@ for (var i = 0; i < pacientes.length; i++) {
   if (pesoValido && alturaValida) {
     var imc = calculaImc(peso, altura);
     resultado.textContent = imc;
+  }
+}
+
+function validaPeso(peso) {
+  if (peso >= 0 && peso < 1000) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validaAltura(altura) {
+  if (altura >= 0 && altura <= 3.0) {
+    return true;
+  } else {
+    return false;
   }
 }
 
