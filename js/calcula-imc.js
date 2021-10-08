b2b1a0d209f555c7619114531a787b61682e948c
Diego Minelli <diego.minelli@hotmail.com>
2021-10-08 14:10:28 -0300

    Criando a funcao calculaImc

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -32,7 +32,15 @@ for (var i = 0; i < pacientes.length; i++) {
   }
 
   if (pesoValido && alturaValida) {
-    var imc = peso / (altura * altura);
-    resultado.textContent = imc.toFixed(2);
+    var imc = calculaImc(peso, altura);
+    resultado.textContent = imc;
   }
+}
+
+function calculaImc(peso, altura) {
+  var imc = 0;
+
+  imc = peso / (altura * altura);
+
+  return imc.toFixed(2);
 }
