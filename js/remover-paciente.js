31d8b67876d78bab142269426d7b8bfd6dea89cd
Diego Minelli <diego.minelli@hotmail.com>
2021-10-11 18:00:49 -0300

    Colocando animacao ao remover paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,16 +1,9 @@
-var pacientes = document.querySelectorAll('.paciente');
-
 var tabela = document.querySelector('table');
 
 tabela.addEventListener('dblclick', function (event) {
-  var alvoEvento = event.target;
-  var paiDoAlvo = alvoEvento.parentNode;
+  event.target.parentNode.classList.add('fadeOut');
 
-  paiDoAlvo.remove();
+  setTimeout(function () {
+    event.target.parentNode.remove();
+  }, 500);
 });
-
-// pacientes.forEach(function (paciente) {
-//   paciente.addEventListener('dblclick', function () {
-//     this.remove();
-//   });
-// });
