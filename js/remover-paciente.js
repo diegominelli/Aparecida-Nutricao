78dae4a2bfd1ea4e60c481b7a9cf7c9d6045fc66
Diego Minelli <diego.minelli@hotmail.com>
2021-10-11 17:46:56 -0300

    Criando a funcao para remover paciente

---
+++ js/remover-paciente.js
@@ -0,0 +1,16 @@
+var pacientes = document.querySelectorAll('.paciente');
+
+var tabela = document.querySelector('table');
+
+tabela.addEventListener('dblclick', function (event) {
+  var alvoEvento = event.target;
+  var paiDoAlvo = alvoEvento.parentNode;
+
+  paiDoAlvo.remove();
+});
+
+// pacientes.forEach(function (paciente) {
+//   paciente.addEventListener('dblclick', function () {
+//     this.remove();
+//   });
+// });
